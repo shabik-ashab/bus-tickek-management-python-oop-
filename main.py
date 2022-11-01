36cf207f7e94e49279478c23236a9babb289eec4
nub69 <shabik.ashab@gmail.com>
2022-11-01 14:12:05 +0600

    MyCompany class initialized

--- main.py
+++ main.py
@@ -16,3 +16,27 @@ class Bus:
         self.from_des = from_des
         self.to = to
         self.seat = ["Empty" for i in range(20)]
+
+class MyCompany: #bus install for admin
+    total_bus = 5
+    total_bus_lst = [] #dummy database
+    
+    def install(self):
+        bus_no = int(input("Enter bus number: "))
+        flag = 1
+        for bus in self.total_bus_lst: #checking if the bus is already on the road
+            if bus_no == bus['coach']:
+                print("Bus already installed")
+                flag = 0
+                break
+        if flag:
+            bus_driver = input("Enter Bus driver name: ")
+            bus_arrival = input("Enter Bus arrival time: ")
+            bus_depurture = input("Enter Bus deperture time: ")
+            bus_form = input("Enter Bus start from: ")
+            bus_to = input("Enter Bus destination: ")
+            self.new_bus = Bus(bus_no, bus_driver, bus_arrival,bus_depurture,bus_form,bus_to)
+
+
+
+
