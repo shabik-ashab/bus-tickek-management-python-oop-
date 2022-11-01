4335423086597a95812a0ea1a7a7a6b761e05bf9
nub69 <shabik.ashab@gmail.com>
2022-11-01 12:17:58 +0600

    Bus class initialized

--- main.py
+++ main.py
@@ -7,3 +7,12 @@ class User:
         self.username = username
         self.password = password
 
+class Bus:
+    def __init__(self,coach,driver,arrival,departure,from_des,to):
+        self.coach = coach
+        self.driver = driver
+        self.arrival = arrival
+        self.departure = departure
+        self.from_des = from_des
+        self.to = to
+        self.seat = ["Empty" for i in range(20)]
