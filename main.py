7389a14ad6563c5521cd31979bce2a9493566822
nub69 <shabik.ashab@gmail.com>
2022-11-02 10:29:14 +0600

    BusCounter initialized

--- main.py
+++ main.py
@@ -3,11 +3,12 @@
 #bus ticket reservation system
 
 class User:
-    def __init__(self,username,password);
+    def __init__(self,username,password):
         self.username = username
         self.password = password
 
 class Bus:
+    bus_seat = 20
     def __init__(self,coach,driver,arrival,departure,from_des,to):
         self.coach = coach
         self.driver = driver
@@ -15,7 +16,7 @@ class Bus:
         self.departure = departure
         self.from_des = from_des
         self.to = to
-        self.seat = ["Empty" for i in range(20)]
+        self.seat = ["Empty" for i in range(self.bus_seat)]
 
 class MyCompany: #bus install for admin
     total_bus = 5
@@ -36,7 +37,30 @@ class MyCompany: #bus install for admin
             bus_form = input("Enter Bus start from: ")
             bus_to = input("Enter Bus destination: ")
             self.new_bus = Bus(bus_no, bus_driver, bus_arrival,bus_depurture,bus_form,bus_to)
+            self.total_bus_lst.append(vars(self.new_bus))
+            print("\nBus installed succesfully")
 
+
+class BusCounter(MyCompany, Bus):
+    user_lst = [] #user database
+    def reservation(self):
+        bus_no = input("Enter Bus Number: ")
+        for bus in self.total_bus_lst:
+            if bus_no == bus['coach']:
+                passenger = input("Enter Your Name: ")
+                seat_no = input("Enter Your Seat Number: ")
+                if seat_no >= self.bus_seat:
+                    print(f"only {self.bus_seat} is avilable")
+                elif bus['seat'][seat_no - 1] != 'Empty':
+                    print("Seat already booked")
+                else:
+                    bus['seat'][seat_no - 1] = passenger
+        
+
+
+
+company = MyCompany()
+company.install()
 
 
 
